e20cb4b71e7e6aa26d1e480ab09edb8e41ee5243
Hening <hening.dev@gmail.com>
2024-04-23 12:09:07 -0300

    update inicial

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+// TRANSIÇÃO DO MENU DE NAVEGAÇÃO AO ROLAR
+window.addEventListener('scroll', function () {
+    var cabecalho = document.getElementById('cabecalho');
+    var cabecalho__logo = document.getElementById('cabecalho__logo');
+    var altura = window.scrollY;
+
+    if (altura > 0) {
+        cabecalho.classList.add('fixed');
+        cabecalho__logo.classList.add('fixed');
+    } else {
+        cabecalho__logo.classList.remove('fixed');
+        cabecalho.classList.remove('fixed');
+    }
+});
+// EXIBIÇÃO PALAVRA CHAVE CARDS SOBRE MIM
+function showMessage() {
+    document.getElementById("mensagem").style.visibility = "visible";
+}
+function hideMessage() {
+    document.getElementById("mensagem").style.visibility = "hidden";
+}
+
+// LINKS DOS PROJETOS
+document.getElementById('petlife').addEventListener("click", function () {
+    window.open("https://pet-life-gamma.vercel.app/", "_blank");
+})
+document.getElementById('jogosecreto').addEventListener("click", function () {
+    window.open("https://jogo-al8wvi6bx-heningdevs-projects.vercel.app/", "_blank");
+})
+document.getElementById('portfolio').addEventListener("click", function () {
+    window.location.href = "#apresentacao";
+})
+
+
