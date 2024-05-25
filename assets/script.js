dc51f93b548b3221eb8c748991cfab02db444b74
Hening <hening.dev@gmail.com>
2024-05-25 03:11:34 -0300

    correção

--- assets/script.js
+++ assets/script.js
@@ -18,20 +18,20 @@ themeBtn.addEventListener('click', () => {
     }
 });
 
-//ALEATORIEDADE DE IMAGEM DO TOPO
-// document.addEventListener('DOMContentLoaded', function() {
-//     const images = [
-//         `../assets/img/fundo.jpg`,
-//         `../assets/img/fundo1.jpg`,
-//         `../assets/img/fundo2.jpg`,
-//         `../assets/img/fundo3.jpg`
-//     ];
+// ALEATORIEDADE DE IMAGEM DO TOPO
+document.addEventListener('DOMContentLoaded', function() {
+    const images = [
+        "../assets/img/fundo.jpg",
+        "../assets/img/fundo1.jpg",
+        "../assets/img/fundo2.jpg",
+        "../assets/img/fundo3.jpg"
+    ];
 
-//     const randomImage = images[Math.floor(Math.random() * images.length)];
+    const randomImage = images[Math.floor(Math.random() * images.length)];
 
-//     const bannerElement = document.getElementById('apresentacao');
-//     bannerElement.style.backgroundImage = `url(${randomImage})`;
-// });
+    const bannerElement = document.getElementById('apresentacao');
+    bannerElement.style.backgroundImage = `url(${randomImage})`;
+});
 
 // TRANSIÇÃO DO MENU DE NAVEGAÇÃO AO ROLAR
 window.addEventListener('scroll', function () {
